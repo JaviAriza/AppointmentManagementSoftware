1a7fe4c48fb4dc00ab475260cdca590135053320
mortiZimmerman <mauricionicolasortiz@gmail.com>
2025-02-23 02:21:26 +0100

    Fix Log in and Register

--- database/migrations/2025_02_21_113124_create_table_mechanics_table.php
+++ database/migrations/2025_02_21_113124_create_table_mechanics_table.php
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('table_mechanics');
+        Schema::dropIfExists('mechanics');
     }
 };
