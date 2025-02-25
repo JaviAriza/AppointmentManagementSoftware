11078fec0241c000bf1e633e1681aee677c31efa
mortiZimmerman <mauricionicolasortiz@gmail.com>
2025-02-26 00:21:56 +0100

    chore: clean up comments and improve code readability

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -19,7 +19,7 @@ class LoginController extends Controller
             'password' => 'required',
         ]);
 
-        // Buscar en la tabla 'client'
+  
         $client = Client::where('email', $request->email)->first();
 
         if ($client && Hash::check($request->password, $client->password)) {
@@ -27,7 +27,7 @@ class LoginController extends Controller
             return redirect()->intended('/dashboard-client');
         }
 
-        // Si no se encuentra en 'client', buscar en la tabla 'mechanic'
+     
         $mechanic = Mechanic::where('email', $request->email)->first();
 
         if ($mechanic && Hash::check($request->password, $mechanic->password)) {
@@ -35,7 +35,7 @@ class LoginController extends Controller
             return redirect()->intended('/dashboard-mechanic');
         }
 
-        // Si no se encuentra en ninguna de las dos tablas
+
         return redirect()->back()->withErrors(['email' => 'Credenciales no válidas']);
     }
 
@@ -43,12 +43,12 @@ class LoginController extends Controller
 
     public function logout(Request $request)
     {
-        Auth::logout(); // Cierra la sesión del usuario
+        Auth::logout();
 
-        $request->session()->invalidate(); // Invalida la sesión
-        $request->session()->regenerateToken(); // Regenera el token CSRF para seguridad
+        $request->session()->invalidate(); 
+        $request->session()->regenerateToken();
 
-        return redirect('/login'); // Redirige a la página de login
+        return redirect('/login');
     }
 
 }
